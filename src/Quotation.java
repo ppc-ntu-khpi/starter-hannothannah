ec7b76119b440eab5dc5243d44507e2cc52f6dac
hannothannah <137100038+hannothannah@users.noreply.github.com>
2024-01-24 16:41:34 +0300

    Commit

---
+++ src/Quotation.java
@@ -0,0 +1,6 @@
+public class Quotation {
+    String quote = "Хоч нам і сумно, але ми все одно повинні жити далі, навіть із цим сумом. — Kaneki Ken";
+  public void display() {
+    System.out.println(quote);
+  }
+}
